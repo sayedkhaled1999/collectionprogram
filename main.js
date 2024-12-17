2f90afe5a02ec02932c4243a5531a91b535d8c13
sayedkhaled1999 <sayedkhaled1999@yahoo.com>
2024-12-17 15:07:49 +0200

    all

--- main.js
+++ main.js
@@ -6,32 +6,30 @@ function createWindow() {
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false, // إذا كنت تستخدم nodeIntegration
+            nodeIntegration: false,
+            contextIsolation: true,
+            webSecurity: true,
         },
     });
 
-    // قم بتحميل الصفحة الرئيسية
     win.loadFile(path.join(__dirname, 'collectionprogram', 'index.html'));
+    win.webContents.openDevTools();
+    win.maximize();
 }
 
-// إعداد IPC للتنقل بين الصفحات
 ipcMain.on('load-page', (event, page) => {
     const win = BrowserWindow.getFocusedWindow();
     win.loadFile(path.join(__dirname, 'collectionprogram', page));
 });
 
-// تشغيل التطبيق
 app.whenReady().then(createWindow);
 
-// التحقق من وجود النوافذ المفتوحة عند إغلاق التطبيق
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-// إعادة إنشاء النافذة عند النقر على أيقونة التطبيق (macOS)
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
