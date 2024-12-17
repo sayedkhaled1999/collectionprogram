dea57a32235cb722ecad9e83469dd74089cb28a2
sayedkhaled1999 <sayedkhaled1999@yahoo.com>
2024-12-17 15:30:15 +0200

    aa

--- main.js
+++ main.js
@@ -1,37 +1,41 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
-function createWindow() {
+async function createWindow() {
+  try {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
-            webSecurity: true,
-        },
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: false,
+        contextIsolation: true,
+        webSecurity: true,
+      },
     });
 
-    win.loadFile(path.join(__dirname, 'collectionprogram', 'index.html'));
+    await win.loadURL(`file://${path.join(__dirname, 'collectionprogram', 'index.html')}`);
     win.webContents.openDevTools();
     win.maximize();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 ipcMain.on('load-page', (event, page) => {
-    const win = BrowserWindow.getFocusedWindow();
-    win.loadFile(path.join(__dirname, 'collectionprogram', page));
+  const win = BrowserWindow.getFocusedWindow();
+  win.loadURL(`file://${path.join(__dirname, 'collectionprogram', page)}`);
 });
 
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
